fa56c4b034e6c1a2f3775612a0424708fea89ca5
WinterLimited <chami0205@gmail.com>
2023-05-23 20:36:39 +0900

    useState refactoring

--- src/components/PlacesPage.tsx
+++ src/components/PlacesPage.tsx
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Spin, Button } from 'antd';
 import RestaurantCard from './RestaurantCard';
 import { fetchRestaurantsFromAPI } from './api';
 import { loadUserVectorFromLocalStorage, calculateCosineSimilarity } from './utils';
@@ -12,53 +13,53 @@ export interface Restaurant {
     cosineSimilarity?: number;
 }
 
-class PlacesPage extends Component {
-    state = {
-        restaurants: [] as Restaurant[],
-        userLatitude: null as number | null,
-        userLongitude: null as number | null,
-    };
+const PlacesPage: React.FC = () => {
+    const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [userLatitude, setUserLatitude] = useState<number | null>(null);
+    const [userLongitude, setUserLongitude] = useState<number | null>(null);
 
-    componentDidMount() {
+    useEffect(() => {
+        fetchUserLocation();
+    }, []);
+
+    const fetchUserLocation = () => {
+        setLoading(true);
         navigator.geolocation.getCurrentPosition(
             (position) => {
-                this.setState(
-                    {
-                        userLatitude: position.coords.latitude,
-                        userLongitude: position.coords.longitude,
-                    },
-                    this.fetchRestaurants
-                );
+                setUserLatitude(position.coords.latitude);
+                setUserLongitude(position.coords.longitude);
+                fetchRestaurants(position.coords.latitude, position.coords.longitude);
             },
             (error) => {
                 console.log('Error:', error);
             }
         );
-    }
+    };
 
-    fetchRestaurants = () => {
-        const { userLatitude, userLongitude } = this.state;
+    const fetchRestaurants = (latitude: number, longitude: number) => {
         const apiKey = process.env.REACT_APP_API_KEY;
+        setLoading(true);
 
-        if (userLatitude && userLongitude && apiKey) {
-            fetchRestaurantsFromAPI(userLatitude, userLongitude, apiKey)
+        if (latitude != null && longitude != null && apiKey) {
+
+            fetchRestaurantsFromAPI(latitude, longitude, apiKey)
                 .then((response) => {
                     const userVector = loadUserVectorFromLocalStorage();
                     const restaurantData = response.data.results;
 
                     let sortedRestaurants;
 
+                    // userVector가 설정되지 않았으면 기본정렬
                     if (userVector.length === 0) {
-                        // 최초 검색, 별도의 유사도 검증 없이 음식점 나열
-                        // If user vector is empty, sort restaurants based on a default order
                         sortedRestaurants = restaurantData.sort((a, b) => {
-                            // Implement your sorting logic here
-                            // For example, sort by rating in descending order
                             return b.rating - a.rating;
                         });
                     } else {
-                        // Calculate cosine similarity and sort restaurants based on similarity
-                        const restaurantDataWithSimilarity = calculateCosineSimilarity(userVector, restaurantData);
+                        const restaurantDataWithSimilarity = calculateCosineSimilarity(
+                            userVector,
+                            restaurantData
+                        );
                         sortedRestaurants = restaurantDataWithSimilarity.sort((a, b) => {
                             if (b.cosineSimilarity && a.cosineSimilarity) {
                                 return b.cosineSimilarity - a.cosineSimilarity;
@@ -67,16 +68,18 @@ class PlacesPage extends Component {
                         });
                     }
 
-                    this.setState({ restaurants: sortedRestaurants });
+                    setRestaurants(sortedRestaurants);
+                    setLoading(false);
                 })
                 .catch((error) => {
                     console.log('Error:', error);
+                    setLoading(false);
                 });
         }
     };
 
 
-    handleSelectRestaurant = (restaurant: Restaurant) => {
+    const handleSelectRestaurant = (restaurant: Restaurant) => {
         console.log('seleRest');
         const selectedRestaurantDetails = {
             price_level: restaurant.price_level,
@@ -87,22 +90,30 @@ class PlacesPage extends Component {
         localStorage.setItem('selectedRestaurant', JSON.stringify(selectedRestaurantDetails));
     };
 
-    render() {
-        const { restaurants } = this.state;
+    const handleRecommendAgain = () => {
+        fetchUserLocation();
+    };
 
-        return (
-            <div>
-                <h2>Recommended Restaurants</h2>
-                {restaurants.map((restaurant, index) => (
+    return (
+        <div>
+            <Button type="primary" onClick={handleRecommendAgain}>
+                재추천
+            </Button>
+            {loading ? (
+                <Spin tip="Loading" size="large">
+                    <div className="content" style={{ padding: '50px', background: 'rgba(0, 0, 0, 0.05)', borderRadius: '4px' }} />
+                </Spin>
+            ) : (
+                restaurants.map((restaurant, index) => (
                     <RestaurantCard
                         key={index}
                         restaurant={restaurant}
-                        onSelect={this.handleSelectRestaurant}
+                        onSelect={handleSelectRestaurant}
                     />
-                ))}
-            </div>
-        );
-    }
-}
+                ))
+            )}
+        </div>
+    );
+};
 
 export default PlacesPage;
